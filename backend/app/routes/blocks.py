84a0fcf281641dafbe33bfe7fd340c37cad1f5c4
liviaalvzs <liv.alvessantana@gmail.com>
2025-03-15 10:37:14 -0300

    feat(routes/blocks): route to delete block

--- backend/app/routes/blocks.py
+++ backend/app/routes/blocks.py
@@ -4,10 +4,14 @@ from .. import crud, schemas, database
 
 router = APIRouter()
 
-@router.post("/blocks/", response_model=schemas.BlockResponse)
+@router.post('/blocks/', response_model=schemas.BlockResponse)
 def create_block(block: schemas.BlockCreate, db: Session = Depends(database.get_db)):
     return crud.create_block(db, block)
 
-@router.get("/blocks/", response_model=list[schemas.BlockResponse])
+@router.get('/blocks/', response_model=list[schemas.BlockResponse])
 def get_blocks(db: Session = Depends(database.get_db)):
     return crud.get_blocks(db)
+
+@router.delete('/blocks/{block_id}/', response_model=dict)
+def delete_block(block_id: int, db: Session = Depends(database.get_db)):
+    return crud.delete_block(db, block_id)
